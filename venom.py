b504d39f3a45595b15df1ef398b81d61cdfff137
Xelciuz <162881232+Xelciuz@users.noreply.github.com>
2024-09-30 23:43:44 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7959745702:AAHHN08QUlwZvIEU46LIRNbQXPnGE2iifVk'
+ADMIN_ID = '6230326750'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
